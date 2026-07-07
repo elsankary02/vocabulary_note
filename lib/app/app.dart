b30634dc468cc92431712e39df4345d94ae63c9a
elsankry02 <elsankry02@gmail.com>
2026-07-07 23:50:05 +0300

    ref2

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,10 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 class NoteApp extends StatelessWidget {
   const NoteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: App());
+    return MaterialApp(debugShowCheckedModeBanner: false);
   }
 }
